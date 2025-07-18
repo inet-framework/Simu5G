5c8088f2acae3bdbe1b72f9a914d25410176a04d
Andras Varga <andras@omnetpp.org>
2025-07-18 09:38:10 +0200

    LteAirFrame: migrated into .msg

--- src/simu5g/stack/phy/packet/LteAirFrame.h
+++ src/simu5g/stack/phy/packet/LteAirFrame.h
@@ -12,51 +12,7 @@
 #ifndef _LTE_LTEAIRFRAME_H_
 #define _LTE_LTEAIRFRAME_H_
 
-#include "simu5g/common/LteCommon.h"
-#include "simu5g/common/LteControlInfo.h"
 #include "simu5g/stack/phy/packet/LteAirFrame_m.h"
-
-namespace simu5g {
-
-using namespace omnetpp;
-
-class LteAirFrame : public LteAirFrame_Base
-{
-  protected:
-    RemoteUnitPhyDataVector remoteUnitPhyDataVector;
-
-  public:
-    LteAirFrame(const char *name = nullptr, int kind = 0) :
-        LteAirFrame_Base(name, kind)
-    {
-    }
-
-    LteAirFrame(const LteAirFrame& other) :
-        LteAirFrame_Base(other)
-    {
-        operator=(other);
-    }
-
-    LteAirFrame& operator=(const LteAirFrame& other)
-    {
-        LteAirFrame_Base::operator=(other);
-        this->remoteUnitPhyDataVector = other.remoteUnitPhyDataVector;
-       ASSERT(other.getControlInfo() == nullptr);
-        return *this;
-    }
-
-    LteAirFrame *dup() const override
-    {
-        return new LteAirFrame(*this);
-    }
-
-    void addRemoteUnitPhyDataVector(RemoteUnitPhyData data);
-    RemoteUnitPhyDataVector getRemoteUnitPhyDataVector();
-};
-
-Register_Class(LteAirFrame);
-
-} //namespace
 
 #endif
 
