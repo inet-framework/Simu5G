459066c3910a30aa0b638ae8b1ddc1035592e69e
Andras Varga <andras@omnetpp.org>
2025-09-22 11:33:38 +0200

    _scripts/run_simulation_comparison.sh improved

--- _scripts/run_simulation_comparison.sh
+++ _scripts/run_simulation_comparison.sh
@@ -3,8 +3,8 @@
 set -e  # Exit on first error
 
 # Configuration
-SIM_PATH="simulations/nr/test_numerology"
-SIM_CONFIG="MultiCell-CBR-DL"
+SIM_PATH="simulations/nr/mec/multiOperator/"
+SIM_ARGS="-c MultiOperator_SingleMec -r 0"
 SIMTIME_LIMIT=5s
 
 run_simulation() {
@@ -15,18 +15,22 @@ run_simulation() {
     git checkout $commit
     make cleanall && make makefiles && make MODE=debug -j12
     cd $SIM_PATH
-    simu5g_dbg -c $SIM_CONFIG -r 0 --sim-time-limit=$SIMTIME_LIMIT \
-       -u Cmdenv --cmdenv-express-mode=false --cmdenv-log-simulation=false \
-       --cmdenv-output-file='${resultdir}/cmdenv.out' --cmdenv-redirect-output=true --result-dir=$results_suffix
+    simu5g_dbg $SIM_ARGS --sim-time-limit=$SIMTIME_LIMIT \
+       -u Cmdenv --cmdenv-express-mode=false --cmdenv-log-simulation=false --cmdenv-redirect-output=true --result-dir=$results_suffix \
+       --cmdenv-output-file='${resultdir}/cmdenv-raw.out' --output-scalar-file='${resultdir}/out.sca'
     cd - > /dev/null
 }
 
 # Run simulations
 run_simulation "before" "results-before"
-run_simulation "topic/maccid" "results-after"
+run_simulation "after" "results-after"
+
+# Strip ANSI coloring sequences from the logs
+cat "$SIM_PATH/results-before/cmdenv-raw.out" | ansifilter -T | sed 's/, id=[0-9]*/, id=REDACTED/' > "$SIM_PATH/results-before/cmdenv.out"
+cat "$SIM_PATH/results-after/cmdenv-raw.out"  | ansifilter -T | sed 's/, id=[0-9]*/, id=REDACTED/' > "$SIM_PATH/results-after/cmdenv.out"
 
 # Diff the results
-echo diff -u "$SIM_PATH/results-before/cmdenv.out"  "$SIM_PATH/results-after/cmdenv.out"
-echo diff -u "$SIM_PATH/results-before/*.sca" "$SIM_PATH/results-after/*.sca"
+echo "diff -u $SIM_PATH/results-before/cmdenv.out  $SIM_PATH/results-after/cmdenv.out | less"
+echo "diff -u $SIM_PATH/results-before/out.sca  $SIM_PATH/results-after/out.sca | less"
 
 echo "Done."
