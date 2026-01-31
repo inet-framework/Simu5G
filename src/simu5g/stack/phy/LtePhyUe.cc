0bb5c31dad87ea778e0212a829f91bf361c2ca2c
Andras Varga <andras@omnetpp.org>
2026-01-31 21:45:38 +0100

    LtePhyUe,HandoverController: method rename: handoverHandler() -> beaconReceived()

--- src/simu5g/stack/phy/LtePhyUe.cc
+++ src/simu5g/stack/phy/LtePhyUe.cc
@@ -193,7 +193,7 @@ void LtePhyUe::handleAirFrame(cMessage *msg)
             return;
         }
 
-        handoverController_->handoverHandler(frame, lteInfo);
+        handoverController_->beaconReceived(frame, lteInfo);
         return;
     }
 
