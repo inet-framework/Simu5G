24b3a4f25d8b4ff9626998a214df795931f8d2fd
Andras Varga <andras@omnetpp.org>
2026-02-24 20:05:38 +0100

    Move markPacket() implementation and data fields from Ip2Nic to TechnologyDecision

--- src/simu5g/stack/ip2nic/Ip2Nic.h
+++ src/simu5g/stack/ip2nic/Ip2Nic.h
@@ -32,22 +32,6 @@ class LteHandoverManager;
 class Ip2Nic : public cSimpleModule
 {
   protected:
-    // Represents a flow using source address, destination address, and type of service.
-    struct FlowKey {
-        uint32_t srcAddr;
-        uint32_t dstAddr;
-        uint16_t typeOfService;
-        bool operator==(const FlowKey& other) const { return srcAddr == other.srcAddr && dstAddr == other.dstAddr && typeOfService == other.typeOfService; }
-    };
-
-    // Hash function for FlowKey
-    struct FlowKeyHash {
-        std::size_t operator()(const FlowKey& key) const {
-            // Combine hashes using XOR and bit shifting for better distribution
-            return std::hash<uint32_t>()(key.srcAddr) ^ (std::hash<uint32_t>()(key.dstAddr) << 1) ^ (std::hash<uint16_t>()(key.typeOfService) << 2);
-        }
-    };
-
     RanNodeType nodeType_;      // UE or NODEB
 
     // reference to the binder
@@ -57,11 +41,6 @@ class Ip2Nic : public cSimpleModule
     MacNodeId nodeId_ = NODEID_NONE;
     // NR MAC node id of this node (if enabled)
     MacNodeId nrNodeId_ = NODEID_NONE;
-
-    // LTE MAC node id of this node's master
-    MacNodeId servingNodeId_ = NODEID_NONE;
-    // NR MAC node id of this node's master (if enabled)
-    MacNodeId nrServingNodeId_ = NODEID_NONE;
 
     // Enable for dual connectivity
     bool dualConnectivityEnabled_;
@@ -103,9 +82,6 @@ class Ip2Nic : public cSimpleModule
     unsigned short drbId_ = 1;
     std::unordered_map<ConnectionKey, DrbId, ConnectionKeyHash> drbIdTable_;
 
-    // for each connection using Split Bearer, keeps track of the number of packets sent down to the PDCP
-    std::unordered_map<FlowKey, int, FlowKeyHash> splitBearersTable_;
-
     cGate *stackGateOut_ = nullptr;       // gate connecting Ip2Nic module to cellular stack
     cGate *ipGateOut_ = nullptr;          // gate connecting Ip2Nic module to network layer
 
@@ -132,9 +108,6 @@ class Ip2Nic : public cSimpleModule
 
   public:
     ~Ip2Nic() override;
-
-    // mark packet for using LTE, NR or split bearer (called by TechnologyDecision)
-    bool markPacket(inet::Ipv4Address srcAddr, inet::Ipv4Address dstAddr, uint16_t typeOfService, bool& useNR);
 };
 
 } //namespace
