6cac926cfc72429591945ac0e2e104b32b12e4a7
Andras Varga <andras@omnetpp.org>
2026-02-25 14:16:23 +0100

    PacketFlowObserver signal refactoring: Step 3 - MAC signals (macPduInserted, macPduAcked, macPduDiscarded, rlcPduDiscarded, grantSent, ulMacPduArrived)

--- src/simu5g/stack/mac/LteMacEnbD2D.cc
+++ src/simu5g/stack/mac/LteMacEnbD2D.cc
@@ -17,7 +17,7 @@
 #include "simu5g/stack/mac/scheduler/LteSchedulerEnbUl.h"
 #include "simu5g/stack/mac/packet/LteSchedulingGrant.h"
 #include "simu5g/stack/mac/conflict_graph/DistanceBasedConflictGraph.h"
-#include "simu5g/stack/packetFlowObserver/PacketFlowObserverBase.h"
+#include "simu5g/stack/packetFlowObserver/PacketFlowSignals.h"
 
 namespace simu5g {
 
@@ -129,8 +129,8 @@ void LteMacEnbD2D::macPduUnmake(cPacket *cpkt)
 
     // Notify the packet flow manager about the successful arrival of a TB from a UE.
     // From ETSI TS 138314 V16.0.0 (2020-07)
-    if (packetFlowObserver_ != nullptr)
-        packetFlowObserver_->ulMacPduArrived(userInfo->getSourceId(), userInfo->getGrantId());
+    GrantSignalInfo ulInfo(userInfo->getSourceId(), userInfo->getGrantId());
+    emit(ulMacPduArrivedSignal_, &ulInfo);
 
     while (macPdu->hasSdu()) {
         // Extract and send SDU
@@ -270,8 +270,8 @@ void LteMacEnbD2D::sendGrants(std::map<GHz, LteMacScheduleList> *scheduleList)
              *   tSched: the point in time when the UL MAC SDU i is scheduled as
              *   per the scheduling grant provided
              */
-            if (packetFlowObserver_ != nullptr)
-                packetFlowObserver_->grantSent(nodeId, grant->getGrantId());
+            GrantSignalInfo grantInfo(nodeId, grant->getGrantId());
+            emit(grantSentSignal_, &grantInfo);
 
             // send grant to PHY layer
             pkt->insertAtFront(grant);
