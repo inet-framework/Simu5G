4530e5d7b6bd803ca6483dc211591aad2760a413
Andras Varga <andras@omnetpp.org>
2026-02-27 16:27:38 +0100

    Remove IPdcpGateway interface and sendTo* methods: entities are fully decoupled from PdcpMux, communicate only via gates

--- src/simu5g/stack/pdcp/LteRxPdcpEntity.h
+++ src/simu5g/stack/pdcp/LteRxPdcpEntity.h
@@ -18,7 +18,6 @@
 #include "simu5g/stack/pdcp/PdcpRxEntityBase.h"
 #include "simu5g/common/LteControlInfo.h"
 #include "simu5g/common/binder/Binder.h"
-#include "simu5g/stack/pdcp/IPdcpGateway.h"
 
 namespace simu5g {
 
@@ -39,9 +38,6 @@ class LteRxPdcpEntity : public PdcpRxEntityBase
     static simsignal_t sentPacketToUpperLayerSignal_;
 
   protected:
-    // reference to the PDCP gateway (for sending packets via output gates)
-    IPdcpGateway *pdcp_ = nullptr;
-
     // Modules references
     inet::ModuleRefByPar<Binder> binder_;
 
