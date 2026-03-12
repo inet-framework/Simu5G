9946cad19da6bc0ce44babebbc5849834e01121f
Andras on behalf of Esteban Egea Lopez <andras@omnetpp.org>
2026-03-12 11:08:58 +0100

    Misc small fixes: BackgroundScheduler init order, LtePhyBase channelModel_ type, Binder.h include, LtePhyEnb debug flag, LcgScheduler TODO

--- src/simu5g/stack/phy/LtePhyBase.h
+++ src/simu5g/stack/phy/LtePhyBase.h
@@ -70,7 +70,7 @@ class LtePhyBase : public ChannelAccess
      */
     static short airFramePriority_;
     /** channel models to use.*/
-    std::map<GHz, opp_component_ptr<LteChannelModel>> channelModel_;
+    std::map<GHz, LteChannelModel*> channelModel_;
     inet::ModuleRefByPar<LteChannelModel> primaryChannelModel_;
 
     /** The id of the in-data gate from the Stack */
@@ -139,7 +139,13 @@ class LtePhyBase : public ChannelAccess
         return primaryChannelModel_;
     }
 
-    const std::map<GHz, opp_component_ptr<LteChannelModel>>& getChannelModels()
+    /*
+    const std::map<double, opp_component_ptr<LteChannelModel>> *getChannelModels()
+    {
+        return &channelModel_;
+    }
+    */
+    const std::map<GHz, LteChannelModel*> &getChannelModels()
     {
         return channelModel_;
     }
