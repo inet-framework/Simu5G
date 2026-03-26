ac0b8613aac21b542a2d4fac6cc80566d2528baf
Andras Varga <andras@omnetpp.org>
2026-03-26 13:14:59 +0100

    Replace "Authors:..." with more precise copyright lines

--- src/simu5g/apps/voip/VoipSender.cc
+++ src/simu5g/apps/voip/VoipSender.cc
@@ -3,8 +3,10 @@
 //
 // Copyright (C) 2012 Antonio Virdis, Daniele Migliorini, Matteo Maria Andreozzi,
 //   Giovanni Accongiagioco, Generoso Pagano, Vincenzo Pii (SimuLTE)
-// Authors: Giovanni Nardini, Giovanni Stea, Antonio Virdis (University of Pisa)
+// Copyright (C) 2013-2021 Giovanni Nardini, Giovanni Stea, Antonio Virdis (University of Pisa)
+// Copyright (C) 2022-2026 Giovanni Nardini, Giovanni Stea (University of Pisa)
 // Editor: Mohamed Seliem (University College Cork)
+//
 // This file is part of a software released under the license included in file
 // "license.pdf". Please read LICENSE and README files before using it.
 // The above files and the present reference are part of the software itself,
