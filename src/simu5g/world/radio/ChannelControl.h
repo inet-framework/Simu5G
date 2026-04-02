9b9b52d6d061c2f1c107efe4fd85ec8cf57f1d42
Andras Varga <andras@omnetpp.org>
2026-04-02 11:29:01 +0200

    added "et al"

--- src/simu5g/world/radio/ChannelControl.h
+++ src/simu5g/world/radio/ChannelControl.h
@@ -3,7 +3,7 @@
 //
 // Copyright (C) 2006 Levente Meszaros, 2005 Andras Varga
 // Copyright (C) 2019-2021 Giovanni Nardini, Giovanni Stea, Antonio Virdis et al. (University of Pisa)
-// Copyright (C) 2022-2026 Giovanni Nardini, Giovanni Stea (University of Pisa)
+// Copyright (C) 2022-2026 Giovanni Nardini, Giovanni Stea et al. (University of Pisa)
 //
 // This file is part of a software released under the license included in file
 // "license.pdf". Please read LICENSE and README files before using it.
