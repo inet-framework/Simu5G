c905a7ae9c77469b757b66b215d2c9381e485394
Andras Varga <andras@omnetpp.org>
2026-07-29 15:56:11 +0200

    src: copyright headers adjusted, IRlcAmEntities.ned split up

--- src/simu5g/stack/pdcp/DcMux.ned
+++ src/simu5g/stack/pdcp/DcMux.ned
@@ -1,7 +1,7 @@
 //
 //                  Simu5G
 //
-// Authors: Giovanni Nardini, Giovanni Stea, Antonio Virdis (University of Pisa)
+// Authors: Andras Varga (OpenSim Ltd)
 //
 // This file is part of a software released under the license included in file
 // "license.pdf". Please read LICENSE and README files before using it.
--- src/simu5g/stack/rlc/RlcRxEntityBase.h
+++ src/simu5g/stack/rlc/RlcRxEntityBase.h
@@ -1,3 +1,14 @@
+//
+//                  Simu5G
+//
+// Authors: Giovanni Nardini, Giovanni Stea, Antonio Virdis (University of Pisa), Andras Varga (OpenSim Ltd)
+//
+// This file is part of a software released under the license included in file
+// "license.pdf". Please read LICENSE and README files before using it.
+// The above files and the present reference are part of the software itself,
+// and cannot be removed from it.
+//
+
 #ifndef _RLC_RX_ENTITY_BASE_H_
 #define _RLC_RX_ENTITY_BASE_H_
 
